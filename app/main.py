32cfeb669daac7067a90dbdebbcfaeec2933e6b5
MariaJwenat <mmjwainat22@cit.just.edu.jo>
2026-04-07 20:47:13 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,12 +4,8 @@ from app.routes.logs import router as logs_router
 
 app = FastAPI(title="Mini SIEM")
 
-# Initialize database
 init_db()
-
-# Include logs routes
 app.include_router(logs_router)
-
 
 @app.get("/")
 def root():
