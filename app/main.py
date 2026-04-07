fac3071662541710aaf1553c8ddde72dc15b3c84
MariaJwenat <mmjwainat22@cit.just.edu.jo>
2026-04-07 20:41:24 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,27 +1,16 @@
 from fastapi import FastAPI
 from app.database import init_db
 from app.routes.logs import router as logs_router
-from app.detection_engine import run_detection
+
 app = FastAPI(title="Mini SIEM")
 
 # Initialize database
 init_db()
 
-
-
-# Include routes
+# Include logs routes
 app.include_router(logs_router)
 
-@app.get("/logs/alerts")
-def get_alerts():
-    return {"test": "NEW CODE WORKING"}
 
-    # 🔥 دمج المصدرين
-    all_logs = file_logs + logs_db
-
-    alerts = run_detection(all_logs)
-    return alerts
-@app.get("/logs")
-def get_logs():
-    file_logs = read_logs("logs.txt")
-    return file_logs + logs_db
+@app.get("/")
+def root():
+    return {"message": "Mini SIEM is running"}
