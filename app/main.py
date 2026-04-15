c97cfdd1f54e13967cbe7eeff4111ef73cee4fa1
MariaJwenat <mmjwainat22@cit.just.edu.jo>
2026-04-15 17:59:40 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.database import init_db
 from app.routes.logs import router as logs_router
-from app.utils.log_reader import read_logs
 
 app = FastAPI(title="Mini SIEM")
 
@@ -22,9 +21,3 @@ app.include_router(logs_router)
 def root():
     return {"message": "Mini SIEM is running"}
 
-# -------------------------
-# Test Endpoint
-# -------------------------
-@app.get("/logs/alerts")
-def test_alerts():
-    return {"status": "working"}
